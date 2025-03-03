d1d45bea5f0c98e1e32e5dfb88534ef9f62283be
ajai B <57012100+ajai-cr7@users.noreply.github.com>
2025-03-03 08:31:18 +0530

    Create 1. twoWaysToCreateThreadsInJava.java

---
+++ LLD/Multithreading/java/Notes/1. twoWaysToCreateThreadsInJava.java
@@ -0,0 +1,42 @@
+public class twoWaysToCreateThreadsInJava
+{
+    public static void main(String[] args) {
+
+        //Approach 1 :
+        approach1 t1 = new approach1();
+        t1.start();
+
+        // Approach 2 :
+        Thread t2 = new Thread(new approach2());
+        t2.start();
+    }
+}
+
+class approach1 extends Thread{
+    @Override
+    public void run(){
+        System.out.println("This thread extends Thread class and overrides run method!");
+    }
+}
+
+class approach2 implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("This thread implements runnable interface and overrides run method!");
+    }
+}
+
+/*
+
+    - Two ways to create Thread in java!
+        1.Extend Thread class and override run method!
+            - Write a class which extends Thread class!
+            - override run method.
+            - Create object for that class and call start method!
+        2.Implement Runnable Functional Interface and override run method!
+            - Create a Thread object.
+            - Pass an object of class which implements Runnable Functional interface to the Thread object as constructor.
+            - Call start() on the Thread object.
+
+
+*/
